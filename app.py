4188b39d13a7f51c36b729170d14713dbb01de39
Emily Manchego-Stillwell <emanchego85@gmail.com>
2022-11-16 23:38:53 -0700

    Fixed path to each html file for Heroku.

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    with open('./Possible_Web_Page_Modifications/index.html') as f:
+    with open('index.html') as f:
         return(f.read())
 
 @app.route('/BreedInfo.html')
 def BreedInfo():
-    with open('./Possible_Web_Page_Modifications/index.html') as f:
+    with open('BreedInfo.html') as f:
         return(f.read())    
 
 @app.route('/about.html')
 def about():
-    with open('./Possible_Web_Page_Modifications/index.html') as f:
+    with open('about.html') as f:
         return(f.read())
